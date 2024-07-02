bfc30607f1afff427136c443c03eded8d2e71b1b
Kattafuah <kattafuah@gmail.com>
2024-07-02 20:04:36 +0000

    still trying to kill vpc

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,61 +1,61 @@
-# ################################################################
-# # General
-# ################################################################
-# variable "tags" {
-#   description = "A map of default tags to add to all resources"
-#   type        = map(string)
-#   default     = {}
-# }
+################################################################
+# General
+################################################################
+variable "tags" {
+  description = "A map of default tags to add to all resources"
+  type        = map(string)
+  default     = {}
+}
 
-# variable "name" {
-#   description = "Account name to use in naming resources"
-#   type        = string
-#   default     = "jmdevopsproject"
-# }
+variable "name" {
+  description = "Account name to use in naming resources"
+  type        = string
+  default     = "jmdevopsproject"
+}
 
-# variable "short_region" {
-#   description = "Short form of the AWS region"
-#   type        = string
-# }
+variable "short_region" {
+  description = "Short form of the AWS region"
+  type        = string
+}
 
-# variable "region" {
-#   description = "AWS region"
-#   type        = string
-# }
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
 
-# variable "account_name" {
-# }
+variable "account_name" {
+}
 
-# variable "account_id" {
-# }
+variable "account_id" {
+}
 
-# #################################################################
-# # VPC
-# #################################################################
-# variable "vpc_ip_cidr" {
-#   type = string
-# }
+#################################################################
+# VPC
+#################################################################
+variable "vpc_ip_cidr" {
+  type = string
+}
 
-# variable "vpc_subnets_map" {
-#   type        = map(any)
-#   description = "Map of CIDR-to-subnet associations"
-# }
+variable "vpc_subnets_map" {
+  type        = map(any)
+  description = "Map of CIDR-to-subnet associations"
+}
 
-# #################################################################
-# # Route53
-# #################################################################
-# variable "zone_id" {
-#   type    = string
-#   default = "ZZ0409119JJPH7TF04AWF"
-# }
+#################################################################
+# Route53
+#################################################################
+variable "zone_id" {
+  type    = string
+  default = "ZZ0409119JJPH7TF04AWF"
+}
 
-# variable "zone_name" {
-#   type    = string
-#   default = "ingkwasiattafua.com"
-# }
+variable "zone_name" {
+  type    = string
+  default = "ingkwasiattafua.com"
+}
 
-# variable "domain_name" {
-#   type    = string
-#   default = "ingkwasiattafua.com"
-# }
+variable "domain_name" {
+  type    = string
+  default = "ingkwasiattafua.com"
+}
 
