15ddfc64e018c248d6594afc2a063a15b99c79f6
Kattafuah <kattafuah@gmail.com>
2024-07-02 20:33:30 +0000

    commented out eip and natgateway

--- modules/tf-vpc/main.tf
+++ modules/tf-vpc/main.tf
@@ -192,57 +192,57 @@ locals {
   nat_gateway_ips   = var.reuse_nat_ips ? var.external_nat_ip_ids : try(aws_eip.nat[*].id, [])
 }
 
-# resource "aws_eip" "nat" {
-#   count = local.create_vpc && var.enable_nat_gateway && !var.reuse_nat_ips ? local.nat_gateway_count : 0
+resource "aws_eip" "nat" {
+  count = local.create_vpc && var.enable_nat_gateway && !var.reuse_nat_ips ? local.nat_gateway_count : 0
 
-#   domain = "vpc"
+  domain = "vpc"
 
-#   tags = merge(
-#     {
-#       "Name" = format(
-#         "${var.account_name}-${var.short_region}-NAT_EIP-%s",
-#         element(var.azs, var.single_nat_gateway ? 0 : count.index),
-#       )
-#     },
-#     var.tags,
-#     )
+  tags = merge(
+    {
+      "Name" = format(
+        "${var.account_name}-${var.short_region}-NAT_EIP-%s",
+        element(var.azs, var.single_nat_gateway ? 0 : count.index),
+      )
+    },
+    var.tags,
+    )
 
-#   depends_on = [aws_internet_gateway.tfprojectvpc]
-# }
+  depends_on = [aws_internet_gateway.tfprojectvpc]
+}
 
-# resource "aws_nat_gateway" "tfprojectvpc" {
-#   count = local.create_vpc && var.enable_nat_gateway ? local.nat_gateway_count : 0
+resource "aws_nat_gateway" "tfprojectvpc" {
+  count = local.create_vpc && var.enable_nat_gateway ? local.nat_gateway_count : 0
 
-#   allocation_id = element(
-#     local.nat_gateway_ips,
-#     var.single_nat_gateway ? 0 : count.index,
-#   )
-#   subnet_id = element(
-#     aws_subnet.public[*].id,
-#     var.single_nat_gateway ? 0 : count.index,
-#   )
+  allocation_id = element(
+    local.nat_gateway_ips,
+    var.single_nat_gateway ? 0 : count.index,
+  )
+  subnet_id = element(
+    aws_subnet.public[*].id,
+    var.single_nat_gateway ? 0 : count.index,
+  )
 
-#   tags = merge(
-#     {
-#       "Name" = format(
-#         "${var.account_name}-${var.short_region}-NAT-%s",
-#         element(var.azs, var.single_nat_gateway ? 0 : count.index),
-#       )
-#     },
-#     var.tags,
-#   )
-#   depends_on = [aws_internet_gateway.tfprojectvpc]
-# }
+  tags = merge(
+    {
+      "Name" = format(
+        "${var.account_name}-${var.short_region}-NAT-%s",
+        element(var.azs, var.single_nat_gateway ? 0 : count.index),
+      )
+    },
+    var.tags,
+  )
+  depends_on = [aws_internet_gateway.tfprojectvpc]
+}
 
 
-# resource "aws_route" "private_nat_gateway" {
-#   count = local.create_vpc && var.enable_nat_gateway ? local.nat_gateway_count : 0
+resource "aws_route" "private_nat_gateway" {
+  count = local.create_vpc && var.enable_nat_gateway ? local.nat_gateway_count : 0
 
-#   route_table_id         = element(aws_route_table.private[*].id, count.index)
-#   destination_cidr_block = var.nat_gateway_destination_cidr_block
-#   nat_gateway_id         = element(aws_nat_gateway.tfprojectvpc[*].id, count.index)
+  route_table_id         = element(aws_route_table.private[*].id, count.index)
+  destination_cidr_block = var.nat_gateway_destination_cidr_block
+  nat_gateway_id         = element(aws_nat_gateway.tfprojectvpc[*].id, count.index)
 
-#   timeouts {
-#     create = "5m"
-#   }
-# }
+  timeouts {
+    create = "5m"
+  }
+}
